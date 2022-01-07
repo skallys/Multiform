1ffd86423445e09bf48988f56053915ee838568e
Simon Pinkas <simon@code-think.com>
2022-01-07 11:01:45 +0100

    Map P5 canvas as sphere texture

--- main.js
+++ main.js
@@ -10,12 +10,19 @@ setupCanvasDrawing();
 animate();
 
 function init() {
+  // Renderer
   const rendererElement = document.getElementById("renderer");
   renderer = new THREE.WebGLRenderer({
     antialias: true,
     canvas: rendererElement
   });
 
+  renderer.setSize(
+    rendererElement.clientWidth,
+    rendererElement.clientHeight
+  );
+
+  // Camera
   camera = new THREE.PerspectiveCamera(
     50,
     window.innerWidth / window.innerHeight,
@@ -23,24 +30,31 @@ function init() {
     2000
   );
 
-  camera.position.set(0, 0, 5);
+  camera.position.set(0, 0, 4);
 
+  // Scene
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xa0a0a0);
 
-  material = new THREE.MeshStandardMaterial();
+  // Material
 
-  mesh = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), material);
+  material = new THREE.MeshStandardMaterial({
+    emissive: new THREE.Color(0xffffff)
+  });
+
+  // Mesh
+  mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 25),
+    material
+  );
   mesh.position.set(0, 0, 0);
   scene.add(mesh);
-  renderer.setSize(
-    rendererElement.clientWidth,
-    rendererElement.clientHeight
-  );
 
   // LIGHTS
   const light = new THREE.AmbientLight(0x404040); // soft white light
   scene.add(light);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(directionalLight);
 
   window.addEventListener("resize", onWindowResize);
 }
@@ -55,7 +69,7 @@ function setupCanvasDrawing() {
   // set canvas as material.map (this could be done to any map, bump, displacement etc.)
   setTimeout(function () {
     const drawingCanvas = document.getElementById("drawing-canvas");
-    material.map = new THREE.CanvasTexture(drawingCanvas);
+    material.emissiveMap = new THREE.CanvasTexture(drawingCanvas);
   }, 0);
 
   // set the variable to keep track of when to draw
@@ -69,8 +83,8 @@ function onWindowResize() {
 
 function animate() {
   requestAnimationFrame(animate);
-  mesh.rotation.x += 0.01;
+  // mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.01;
-  material.map.needsUpdate = true;
+  material.emissiveMap.needsUpdate = true;
   renderer.render(scene, camera);
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 import p5 from "p5";
 
 let sketch = function (p) {
+  let offset = 0;
   p.setup = function () {
     let cnv = p.createCanvas(window.innerWidth / 3.33, 400);
+    p.pixelDensity(5);
     cnv.id("drawing-canvas");
-    p.background(0, 0, 0, 1);
   };
   p.draw = function () {
+    p.background(0, 0, 0, 255);
+
+    for (let i = 0; i < p.width; i += 5) {
+      p.stroke(255, 255, 255);
+      p.line(i + offset, 0, i * offset, p.height);
+    }
     p.rect(p.mouseX, p.mouseY, 10, 10);
+    offset += 0.001;
   };
 };
 
