7c5b8c64954d1337d1f773e0b239692962817a00
Simon Pinkas <simon@code-think.com>
2022-01-11 09:45:22 +0100

    Taller select

--- renderer.js
+++
@@ -1,4 +0,0 @@
-import model from "./models/jersey/scene.glb";
-import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
