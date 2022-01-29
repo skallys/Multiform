9610a670c91d7cb1d662cc03417d7860704809fb
Simon Pinkas <simon@code-think.com>
2022-01-29 16:36:50 +0100

    Configure Vite for multipage

---
+++ app/app.js
@@ -0,0 +1,43 @@
+import "./styles/style.css";
+import "./components/sketches/lines";
+import "./components/sketches/random_lines";
+import "./components/sketches/cat";
+import "./components/sketches/small_ellipses";
+import "./components/sketches/rectangles";
+
+import { sketches, switchSketch } from "./components/sketches";
+import { init, setCanvasTexture } from "./components/renderer";
+
+let currentSketch;
+let rendererIsLoaded = init();
+
+if (rendererIsLoaded) {
+  createSelect();
+}
+
+// Create select dropdown
+function createSelect() {
+  // Default sketch
+  currentSketch = switchSketch(0);
+
+  // Get <select> element from DOM
+  const selectElement = document.getElementById("sketch-select");
+
+  sketches.forEach(function (e, i) {
+    // For each sketch in sketches array, create a new option element + text node
+    // from name in object. Use index as the value, for use in switch
+    let element = document.createElement("option");
+    let content = document.createTextNode(e.name);
+    element.appendChild(content);
+    element.value = i;
+    selectElement.appendChild(element);
+    if (currentSketch == e) {
+      element.selected = true;
+    }
+  });
+
+  selectElement.addEventListener("input", function () {
+    currentSketch = switchSketch(this.value);
+    setCanvasTexture();
+  });
+}
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-export default {
-  assetsInclude: ["**/*.glb"]
-};
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  assetsInclude: ["**/*.glb"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        app: resolve(__dirname, "app/index.html")
+      }
+    }
+  }
+});
+
+// export default {
+//   assetsInclude: ["**/*.glb"]
+// };
