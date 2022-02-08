62a087419766b6a13987e5a03538fea9414223f2
skallys <saloua.karib@eduvaud.ch>
2022-02-08 14:58:39 +0100

    SUCCESS animation 'same but different'

--- app/components/renderer.js
+++ app/components/renderer.js
@@ -23,7 +23,7 @@ export const init = () => {
   //saloua: ici pour que le jersey ne soit pas déformé: (window.innerWidth/100)*66.7) /100 + *66.7 c'est pour que celà entre bien dans la fenêtre qui fait 66.7% de la taille de la fenêtre.
   camera = new THREE.PerspectiveCamera(
     50,
-    (((window.innerWidth / 100) * 66.7)-10) / window.innerHeight,
+    ((window.innerWidth / 100) * 66.7) / window.innerHeight,
     1,
     5000
   );
--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 import "./styles/homepage.css";
 import "./styles/common.css";
+
+//0 - 625 = scrolled
+
+const sameBut = document.querySelector('.samebutd');
+const diff = document.querySelector('.sbdifferent');
+
+window.addEventListener('scroll', function(){ 
+    const scrolled = window.pageYOffset;
+    const val = (scrolled * 0.5) + 68.85;
+    diff.style.transform = 'scale(' + (1.45 * val) + '%)'
+
+    const lav = (scrolled * 0.5) + 1;
+    sameBut.style.transform = 'scale(' +  (99.85 / lav)  + '%)'
+    console.log(lav)
+    //sameBut.style.transform = "scale(100 * " + val + ")" ;
+});
+
+
